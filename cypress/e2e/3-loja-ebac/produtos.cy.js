6fa932228443043ba5c95911e851941a5335a32b
Angelo Souza <ang3lokz@gmail.com>
2024-06-18 19:12:23 -0300

    Lista de Produtos

---
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -0,0 +1,20 @@
+///<reference types="cypress"/>
+
+describe('Funcionalidade: Produtos', () => {
+
+    beforeEach(() => {
+        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+    });
+    
+    it('Deve selecionar um produto da lista', () => {
+        cy.get('.product-block')
+            //.first()
+            //.last()
+            //.eq(2)
+            .contains('Ariel Roll Sleeve Sweatshirt')
+            .click()
+            cy.get('#tab-description > :nth-child(1)').should('exist')
+
+        
+    });
+});
